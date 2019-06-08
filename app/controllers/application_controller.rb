dbc27c835a8b09e6ece1862d2eaca888d5585ec5
github username <github email address>
2019-06-08 12:41:53 -0400

    added users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,14 +9,21 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "not_a_secret"
   end
 
-  get "/" do
-    erb :welcome
-  end
-
 get "/"  do
   erb :index
 end
 
+helpers do
+
+  def logged_in?
+    !!session[:username]
+  end
+
+def login(username,password)
+  if user = User.find_by(:username => username) && user.authenticate(password)
+    session[:username] = user.username
+  else
+    redirect '/login'
 
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
 
+get '/signup' do
+  if logged_in?
+    redirect '/cars'
+  else
+  erb :'/users/new.signup'
+end
 
-
+post '/users' do
+@users = Users.new
+@users.username = params[:username]
+@users.password = params[:password]
+if @user.save
+  redirect '/login'
+else
+  erb :'users/new.signup'
+end
+end
 
 end
